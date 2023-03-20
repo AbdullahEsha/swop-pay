53b57a7015621191b365cac0e8994ea85b5a318b
AbdullahIsha <shahariaresha@gmail.com>
2023-03-20 18:27:09 +0600

    innitially complete

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,50 +1,66 @@
 import Image from "next/image";
 import Link from "next/link";
 import { BsPersonCircle } from "react-icons/bs";
+import { useEffect, useState } from "react";
 
 const MainNav = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 20) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll", null);
+    };
+  }, []);
+
   return (
     <>
-      <div className="nav-panel container">
-        <Link href="/">
-          <Image
-            src="/image/swop-pay-logo-white.png"
-            alt="swop-pay-logo-white"
-            height={35}
-            width={219}
-          />
-        </Link>
-        <ul>
-          <li>
-            <Link href="/">Dashboard</Link>
-          </li>
-          <li>
-            <Link href="/">Profile</Link>
-          </li>
-          <li>
-            <Link href="/">Invoice</Link>
-          </li>
-          <li>
-            <Link href="/">Order</Link>
-          </li>
-          <li>
-            <Link href="/">Products</Link>
-          </li>
-          <li>
-            <Link href="/">Coupon</Link>
-          </li>
-          <li>
-            <Link href="/">About</Link>
-          </li>
-          <li>
-            <Link href="/">Contact</Link>
-          </li>
-          <li>
-            <Link href="/">
-              <BsPersonCircle size="25" />
-            </Link>
-          </li>
-        </ul>
+      <div className={`${show && "nav-panel-scroll"} nav-panel-scroll-bar`}>
+        <div className="nav-panel container">
+          <Link href="/" id="logo-swop">
+            <Image
+              src="/image/swop-pay-logo-white.png"
+              alt="swop-pay-logo-white"
+              height={35}
+              width={219}
+            />
+          </Link>
+          <ul>
+            <li>
+              <Link href="/">Dashboard</Link>
+            </li>
+            <li>
+              <Link href="/">Profile</Link>
+            </li>
+            <li>
+              <Link href="/">Invoice</Link>
+            </li>
+            <li>
+              <Link href="/">Order</Link>
+            </li>
+            <li>
+              <Link href="/">Products</Link>
+            </li>
+            <li>
+              <Link href="/">Coupon</Link>
+            </li>
+            <li>
+              <Link href="/">About</Link>
+            </li>
+            <li>
+              <Link href="/">Contact</Link>
+            </li>
+            <li>
+              <Link href="/">
+                <BsPersonCircle size="25" />
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,198 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className=""></div>
+        <div className="container dashboard-div">
+          <div className="row">
+            <div className="col-6 swop-dashboard-content-out">
+              <div className="swop-dashboard-content">
+                <h2>Everything You Can Handle From Your Dashboard</h2>
+                <p>
+                  Swop’s Flat Rectangle NFC’s are designed to be durable and
+                  simple to use. The Flat is great to put under any phone
+                  case(non-metal) Users can download our app to program any of
+                  our NFC’s with the click of a button. This will attach your
+                  profile to the NFC for easy sharing. You just tap the NFC on
+                  any compatible.
+                </p>
+              </div>
+            </div>
+            <div className="col-6">
+              <Image
+                src="/image/swop-dashboard.png"
+                alt="swop-dashboard"
+                height={641}
+                width={635}
+                id="swop-dashboard"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="container home-how-it-work">
+          <h2>HOW ITS WORKS</h2>
+          <p className="hide-mobile-view">
+            Swop is free to download and very easy to get started. After making
+            a profile and getting setup be
+            <br />
+            sure to order a NFC. Our technology is compatible with all current
+            and most
+            <br />
+            modern models of smartphones. Never miss an opportunity
+            <br />
+            again with Swop.
+          </p>
+          <p className="hide-pc-view">
+            Swop is free to download and very easy to get started. After making
+            a profile and getting setup be sure to order a NFC. Our technology
+            is compatible with all current and most modern models of
+            smartphones. Never miss an opportunity again with Swop.
+          </p>
+          <div className="background-level" />
+          <div className="row">
+            <div className="col-4">
+              <Image
+                src="/image/number-icon1.png"
+                alt="number_image"
+                height={71}
+                width={71}
+                className="number-icon"
+              />
+              <div className="card-how-it">
+                <Image
+                  src="/image/homeCard1.png"
+                  alt="no_image"
+                  width={360}
+                  height={540}
+                />
+                <h5>
+                  Set up your <span>SWOP</span> profile
+                </h5>
+                <p>
+                  After creating an account and setting up your wallet, create a
+                  website in the builder. Building a website is easy with our
+                  app, it provides a template to input all your important
+                  information.
+                </p>
+              </div>
+            </div>
+            <div className="col-4">
+              <Image
+                src="/image/number-icon2.png"
+                alt="number_image"
+                height={71}
+                width={71}
+                className="number-icon"
+              />
+              <div className="card-how-it">
+                <Image
+                  src="/image/homeCard2.png"
+                  alt="no_image"
+                  width={360}
+                  height={540}
+                />
+                <h5>
+                  Active <span>NFC</span> device
+                </h5>
+                <p>
+                  After ordering a NFC device and setting up your microsite,
+                  press the activate button on your website builder and tap the
+                  NFC to your phone to connect the NFC to your website.
+                </p>
+              </div>
+            </div>
+            <div className="col-4">
+              <Image
+                src="/image/number-icon3.png"
+                alt="number_image"
+                height={71}
+                width={71}
+                className="number-icon"
+              />
+              <div className="card-how-it">
+                <Image
+                  src="/image/homeCard3.png"
+                  alt="no_image"
+                  width={360}
+                  height={540}
+                />
+                <h5>
+                  Ready to <span>connect</span>
+                </h5>
+                <p>
+                  Tap the NFC and hold from 1-4 seconds on anyone’s phone you
+                  meet and your profile will pop up. The other person does NOT
+                  need the swop app for the NFC to work.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container send-link-div">
+          <div className="row">
+            <div className="col-6">
+              <Image
+                src="/image/swop-pay-image.png"
+                alt="polygon-matic-badge"
+                height={764}
+                width={531}
+                id="polygon-matic-badge"
+              />
+            </div>
+            <div className="col-6 send-link-content-out">
+              <div className="send-link-content">
+                <h2>Send A Link To Get Paid</h2>
+                <p>
+                  Swop’s Flat Rectangle NFC’s are designed to be durable and
+                  simple to use. The Flat is great to put under any phone
+                  case(non-metal) Users can download our app to program any of
+                  our NFC’s with the click of a button. This will attach your
+                  profile to the NFC for easy sharing. You just tap the NFC on
+                  any compatible.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container transaction">
+          <div className="transaction-cost">
+            <Image
+              src="/image/cost-icon.png"
+              alt="cost-icon"
+              height={133}
+              width={133}
+            />
+            <h3>Avarage cost per transaction</h3>
+            <p>
+              Personlize the look of your digital business card. Add all of your
+              contact information.
+            </p>
+          </div>
+          <div className="transaction-time">
+            <Image
+              src="/image/time-icon.png"
+              alt="time-icon"
+              height={133}
+              width={133}
+            />
+            <h3>Avarage time per transaction</h3>
+            <p>
+              Personlize the look of your digital business card. Add all of your
+              contact information.
+            </p>
+          </div>
+          <div className="transaction-daily">
+            <Image
+              src="/image/daily-icon.png"
+              alt="daily-icon"
+              height={133}
+              width={133}
+            />
+            <h3>Avarage daily transaction</h3>
+            <p>
+              Personlize the look of your digital business card. Add all of your
+              contact information.
+            </p>
+          </div>
+        </div>
         <Footer />
       </main>
     </>
